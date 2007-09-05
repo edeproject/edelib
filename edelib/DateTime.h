37a2f8ec5f45790206079b88ce26ac3c1b5707f3
Vedran Ljubovic <vljubovic@users.sf.net>
2007-09-05 07:51:29 +0000

    Fix minor bug in Date::operator>

--- edelib/DateTime.h
+++ edelib/DateTime.h
@@ -262,7 +262,7 @@ inline bool operator==(const Date& d1, const Date& d2)
 
 inline bool operator>(const Date& d1, const Date& d2) {
 	return (d1.year() > d2.year() || (d1.year() == d2.year() && d1.month() > d2.month()) || 
-		(d1.month() == d2.month() && d1.day() > d2.day()));
+		(d1.year() == d2.year() && d1.month() == d2.month() && d1.day() > d2.day()));
 }
 			
 inline bool operator!=(const Date& d1, const Date& d2) { return !(d1 == d2); }
