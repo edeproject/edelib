57e82e37fe0f0c28d7bcc5dd6e910e4f39010428
Sanel Zukan <karijes@users.sf.net>
2008-03-18 12:38:36 +0000

    Merging dbus code into edelib

---
+++ src/EdbusList.cpp
@@ -0,0 +1,98 @@
+#include "EdbusList.h"
+#include <stdio.h>
+
+EdbusList::EdbusList(bool a) : array_mode(a) { }
+
+void EdbusList::append(const EdbusData& val) {
+	if(array_mode && impl->lst.size() > 0) {
+		const_iterator it = begin();
+
+		if((*it).type() != val.type()) {
+			puts("Array contains different types. Ignoring...\n");
+			return;
+		}
+	}
+
+	unhook();
+	impl->lst.push_back(val);
+}
+
+void EdbusList::clear(void) {
+	unhook();
+	impl->lst.clear();
+}
+
+void EdbusList::remove(const EdbusData& val) {
+	unhook();
+
+	EdbusList::iterator it = impl->lst.begin(), it_end = impl->lst.end();
+
+	for(; it != it_end; ++it) {
+		if((*it) == val) {
+			impl->lst.erase(it);
+			return;
+		}
+	}
+}
+
+void EdbusList::remove_all(const EdbusData& val) {
+	unhook();
+
+	EdbusList::iterator it = impl->lst.begin(), it_end = impl->lst.end();
+
+	for(; it != it_end; ++it) {
+		if((*it) == val)
+			it = impl->lst.erase(it);
+	}
+}
+
+bool EdbusList::operator==(const EdbusList& other) const {
+	/* 
+	 * if we are array but not other, there is no
+	 * point to check stuff
+	 */
+	if(array_mode != other.array_mode)
+		return false;
+
+	return impl->lst == other.impl->lst;
+}
+
+EdbusDataType EdbusList::value_type(void) {
+	assert(size() > 0 && "Can't get key type on empty container");
+
+	if(array_mode) {
+		EdbusList::const_iterator it = begin();
+		return (*it).type();
+	}
+
+	return EDBUS_TYPE_INVALID;
+}
+
+bool EdbusList::value_type_is_container(void) {
+	assert(size() > 0 && "Can't get value type on empty container");
+
+	if(array_mode) {
+		EdbusList::const_iterator it = begin();
+		if(EdbusData::basic_type(*it))
+			return false;
+		if((*it).type() == EDBUS_TYPE_VARIANT)
+			return false;
+		return true;
+	}
+
+	/* then is struct */
+	return false;
+}
+
+EdbusList::const_iterator EdbusList::begin(void) const {
+	return impl->lst.begin();
+}
+
+EdbusList::const_iterator EdbusList::end(void) const {
+	return impl->lst.end();
+}
+
+unsigned int EdbusList::size(void) const {
+	return impl->lst.size();
+}
+
