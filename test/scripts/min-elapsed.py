af36552a0aeeb3f0690f33279c42831b3b805364
Sanel Zukan <karijes@users.sf.net>
2009-04-10 11:39:59 +0000

    Removing CDash related scripts

--- test/scripts/min-elapsed.py
+++
@@ -1,7 +0,0 @@
-#
-# Stupid 'expr' command can't handle float values and not everyone has installed 'bc'
-# so calculate elapsed minutes here
-# sz.
-
-import sys
-print (int(sys.argv[1]) - int(sys.argv[2])) * 0.0166666
