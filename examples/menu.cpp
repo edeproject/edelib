a912f745aad8c4208fb80c76ea795bc002d74cfc
Sanel Zukan <karijes@users.sf.net>
2009-07-03 13:21:58 +0000

    Extended a little bit menu example

--- examples/menu.cpp
+++ examples/menu.cpp
@@ -11,8 +11,8 @@ edelib::MenuItem menu_[] = {
  {"S&econd item", 0,  0, 0, 0, FL_NORMAL_LABEL, 0, 12, 0},
  {"Third item", 0,  0, 0, 128, FL_NORMAL_LABEL, 0, 12, 0},
  {"Submenu", 0,  0, 0, 64, FL_NORMAL_LABEL, 0, 12, 0},
- {"item1", 0,  0, 0, 0, FL_NORMAL_LABEL, 0, 12, 0},
- {"item2", 0,  0, 0, 0, FL_NORMAL_LABEL, 0, 12, 0},
+ {"&Item 1", 0,  0, 0, 0, FL_NORMAL_LABEL, 0, 12, 0},
+ {"I&tem 2", 0,  0, 0, 0, FL_NORMAL_LABEL, 0, 12, 0},
  {0,0,0,0,0,0,0,0,0},
  {"&Quit", 0x40071,  0, 0, 0, FL_NORMAL_LABEL, 0, 12, 0},
  {0,0,0,0,0,0,0,0,0},
@@ -30,9 +30,11 @@ int main(int argc, char **argv) {
 
 		Fl_Image* img1 = (Fl_Image*)edelib::IconLoader::get("document-new", edelib::ICON_SIZE_TINY);
 		Fl_Image* img2 = (Fl_Image*)edelib::IconLoader::get("document-save", edelib::ICON_SIZE_SMALL);
+		Fl_Image* img3 = (Fl_Image*)edelib::IconLoader::get("system-log-out", edelib::ICON_SIZE_TINY);
 
 		menu_[1].image(img1);
 		menu_[2].image(img2);
+		menu_[8].image(img3);
 	win->end();
 	win->show(argc, argv);
 	return Fl::run();
